725c20771fdc80c6de325b1b5daf2528558185e1
MostafaCse <mdmostafaiiuc@gmail.com>
2020-10-21 10:19:28 +0600

    uncomplate

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ function App() {
   return (
     <div>
        <h1>due to my eye's problem , i was unable to continue my study.now i am ok, i am doing this  assignment.due to short time, i  am submitting the assignment incomplately.plz consider it.</h1>
-    
+      <h1>title of the mostafa's kindom</h1>
+      <p>here you will find the descripaation of mostafa's kingdom</p>
+      <footer>this is called footer and also read in class seven eigt</footer>
     </div>
   );
 }
